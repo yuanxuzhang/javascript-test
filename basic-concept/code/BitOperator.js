de71f0c2c342cc8c450b9cdecbf532e96bdf56b3
张圆绪 <yuanxu_zhang@126.com>
2018-08-17 21:39:32 +0800

    位操作

---
+++ basic-concept/code/BitOperator.js
@@ -0,0 +1,21 @@
+console.log(~NaN^NaN)
+
+var num = 25;
+// -26
+console.log(~num);
+num = -25;
+// 24
+console.log(~num);
+
+//64
+console.log(2 << 5)
+//-64
+console.log(-2 << 5)
+//2
+console.log(64 >> 5)
+//-2
+console.log(-64 >> 5)
+//2
+console.log(64 >>> 5)
+//134217726
+console.log(-64 >>> 5)

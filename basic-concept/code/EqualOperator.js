27b41f57d031c142f8a8b71704f7a3bf9e4f4079
张圆绪 <yuanxu_zhang@126.com>
2018-08-19 20:46:47 +0800

    Add files via upload

---
+++ basic-concept/code/EqualOperator.js
@@ -0,0 +1,16 @@
+// true
+console.log(null == undefined);
+// false
+console.log("NaN" == NaN);
+// false
+console.log(NaN == NaN);
+// true
+console.log(null == undefined);
+// true
+console.log(NaN != NaN);
+// true
+console.log(false == 0);
+// true
+console.log(true == 1);
+// false
+console.log(undefined == 0);
